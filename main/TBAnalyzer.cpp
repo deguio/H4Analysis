6e671db8793bdc46effafbaf67ccdde652b6340b
federico de guio <federico.de.guio@cern.ch>
2017-08-28 16:56:48 +0200

    analyzer update

--- main/TBAnalyzer.cpp
+++ main/TBAnalyzer.cpp
@@ -27,19 +27,11 @@
 #include "TApplication.h"
 
 
-unsigned int CH0 = 0;
-unsigned int CH1 = 1;
-unsigned int CH2 = 2;
-unsigned int CH3 = 3;
-unsigned int CH8 = 4;
-
 //=== focusing on ch1 for now (bottom left) ===
-unsigned int CH = CH2;
-unsigned int CHref = CH2;
-
 struct TreeVars
 {  
   float t_beamE;
+  float t_HV;
   unsigned int t_nCh;
   
   float* t_beamX;
@@ -63,6 +55,7 @@ struct TreeVars
 void InitTreeVars(TTree* chain1, TreeVars& treeVars)
 {
   chain1 -> SetBranchStatus("Energy",1); chain1 -> SetBranchAddress("Energy",&treeVars.t_beamE);
+  chain1 -> SetBranchStatus("HV",1); chain1 -> SetBranchAddress("HV",&treeVars.t_HV);
   chain1 -> SetBranchStatus("n_channels",1); chain1 -> SetBranchAddress("n_channels",&treeVars.t_nCh);
   
   treeVars.t_beamX = new float[2];
@@ -70,15 +63,15 @@ void InitTreeVars(TTree* chain1, TreeVars& treeVars)
   chain1 -> SetBranchStatus("X",1); chain1 -> SetBranchAddress("X",treeVars.t_beamX);
   chain1 -> SetBranchStatus("Y",1); chain1 -> SetBranchAddress("Y",treeVars.t_beamY);
   
-  treeVars.t_chargeSig = new float[treeVars.t_nCh];
-  treeVars.t_chargeTot = new float[treeVars.t_nCh];
+  treeVars.t_chargeSig = new float[5];
+  treeVars.t_chargeTot = new float[5];
   chain1 -> SetBranchStatus("charge_sig",1); chain1 -> SetBranchAddress("charge_sig",treeVars.t_chargeSig);
   chain1 -> SetBranchStatus("charge_tot",1); chain1 -> SetBranchAddress("charge_tot",treeVars.t_chargeTot);
 
-  treeVars.t_ampMax = new float[treeVars.t_nCh];
-  treeVars.t_timeMax = new float[treeVars.t_nCh];
-  treeVars.t_chi2Max = new float[treeVars.t_nCh];
-  treeVars.t_timeCFD = new float[treeVars.t_nCh];
+  treeVars.t_ampMax = new float[5];
+  treeVars.t_timeMax = new float[5];
+  treeVars.t_chi2Max = new float[5];
+  treeVars.t_timeCFD = new float[5];
   chain1 -> SetBranchStatus("amp_max",1); chain1 -> SetBranchAddress("amp_max",treeVars.t_ampMax);
   chain1 -> SetBranchStatus("time_max",1); chain1 -> SetBranchAddress("time_max",treeVars.t_timeMax);
   chain1 -> SetBranchStatus("chi2_max",1); chain1 -> SetBranchAddress("chi2_max",treeVars.t_chi2Max);
@@ -100,28 +93,34 @@ void InitTreeVars(TTree* chain1, TreeVars& treeVars)
 
 
 //=== cut on the hodo position ===
-bool SelectEvent_hodo(TreeVars treeVars)
+bool SelectEvent_hodo(TreeVars treeVars, std::vector<float> positionCuts)
 {
-  //if( !(treeVars.t_beamX[0]>-13 && treeVars.t_beamX[0]<6 && treeVars.t_beamY[0]>-11 && treeVars.t_beamY[0]<7) ) return false;
-  if( !(treeVars.t_beamX[0]>-6 && treeVars.t_beamX[0]<-2 && treeVars.t_beamY[0]>-4 && treeVars.t_beamY[0]<0) ) return false;
+  if( !(treeVars.t_beamX[0]>positionCuts[0] && treeVars.t_beamX[0]<positionCuts[1] && treeVars.t_beamY[0]>positionCuts[2] && treeVars.t_beamY[0]<positionCuts[3]) ) return false;
   return true;
 }
-bool SelectEnPoints(TreeVars treeVars)
+bool SelectEnPoints(TreeVars treeVars, std::vector<float> enpoints)
 {
-  if( treeVars.t_beamE == 200 ) return false;
-  return true;
+  for(int en=0; en<enpoints.size(); ++en)
+    if(treeVars.t_beamE == enpoints[en])
+      return true;
+
+  return false;
 }
-bool SelectEvent_scint(TreeVars treeVars)
+bool SelectHVPoints(TreeVars treeVars, std::vector<float> hvpoints)
 {
-  if( treeVars.t_ampMax[CH] < 40. ) return false;
-  if( treeVars.t_ampMax[CHref] < 200. ) return false;
-  if( (treeVars.t_timeMax[CH]/0.4-treeVars.t_timeMax[CH8]/0.4) < -240 ) return false;
-  return true;
+  for(int hv=0; hv<hvpoints.size(); ++hv)
+    if(treeVars.t_HV == hvpoints[hv])
+      return true;
+
+  return false;
 }
-bool SelectEvent_kov(TreeVars treeVars)
+bool SelectEvent_ref(TreeVars treeVars, int mainch, float mainthr, std::vector<float> refch, std::vector<float> refthr)
 {
-  if( (treeVars.t_timeMax[CH]/0.4-treeVars.t_timeMax[CH8]/0.4) > -280 ) return false;
-  //if( treeVars.t_ampMax[CH] < 2200. ) return false;
+  if( treeVars.t_ampMax[mainch] < mainthr ) return false;
+
+  for(int ch=0; ch<refch.size(); ++ch)
+    if( treeVars.t_ampMax[int(refch[ch])] < refthr[ch] ) return false;
+
   return true;
 }
 
@@ -130,20 +129,36 @@ bool SelectEvent_kov(TreeVars treeVars)
 
 int main(int argc, char** argv)
 {
+  gSystem -> Load("CfgManager/lib/libCFGMan.so");
   setTDRStyle();
-  TApplication* theApp;
-  theApp = new TApplication("App", &argc, argv);
 
+  if( argc < 2 )
+    {
+      std::cerr << ">>>>> TBAnalyzer.cpp::usage:   " << argv[0] << " configFileName" << std::endl;
+      return 1;
+    }
 
-  std::string fileName = "/eos/cms/store/group/dpg_hcal/comm_hcal/deguio/CeF3_TB_Jul2017/EnScan/ntuples_v2/ntuples_v2_hadd.root";
-  //std::string fileName = "/eos/cms/store/group/dpg_hcal/comm_hcal/deguio/CeF3_TB_Jul2017/EnScan/ntuples_v3/ntuples_v3_hadd.root";
-  //std::string fileName = "./ntuples/H4July2017_test_8412.root";
+  //=== Take Params from Config ===
+  CfgManager opts;
+  opts.ParseConfigFile(argv[1]);
+  
+  std::string fileName = opts.GetOpt<std::string>("Input.inputFile");
+  std::string outName = opts.GetOpt<std::string>("Input.outputFile");
+  std::vector<float> positionCuts = opts.GetOpt<std::vector<float>>("Cuts.position");
+  std::vector<float> enpoints     = opts.GetOpt<std::vector<float>>("Cuts.enpoints");
+  std::vector<float> hvpoints     = opts.GetOpt<std::vector<float>>("Cuts.hvpoints");
 
-  TFile* outFile = new TFile("TBAnalysis.root", "RECREATE");
+  int mainch    = opts.GetOpt<int>("Input.mainch");
+  float mainthr = opts.GetOpt<int>("Input.mainthr");
+  std::vector<float> refch   = opts.GetOpt<std::vector<float>>("Input.refch");
+  std::vector<float> refthr  = opts.GetOpt<std::vector<float>>("Input.refthr");
+  int trgch  = opts.GetOpt<int>("Input.trgch");
+
+  //=== outFile ===
+  TFile* outFile = new TFile(outName.c_str(), "RECREATE");
   outFile -> cd();
 
 
-  
   //=== setup chains ===
   TChain* chain1 = new TChain("info","info");
   TChain* chain2 = new TChain("hodo","hodo");
@@ -188,6 +203,10 @@ int main(int argc, char** argv)
   std::map<float,TH1F*> h_ampMax;
   std::map<float,TProfile*> h_wf;
 
+  TApplication* theApp;
+  theApp = new TApplication("App", &argc, argv);
+
+
   //=== loop over events ===
   int nEntries = chain1 -> GetEntries();
   for(int entry = 0; entry < nEntries; ++entry)
@@ -197,43 +216,48 @@ int main(int argc, char** argv)
     chain1 -> GetEntry(entry);
 
     //=== fill histograms ===
-    h_chargeMap_ch0->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[CH0]);
-    h_chargeMap_ch1->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[CH1]);
-    h_chargeMap_ch2->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[CH2]);
-    h_chargeMap_ch3->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[CH3]);
+    h_chargeMap_ch0->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[0]);
+    h_chargeMap_ch1->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[1]);
+    h_chargeMap_ch2->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[2]);
+    h_chargeMap_ch3->Fill(treeVars.t_beamX[0],treeVars.t_beamY[0],treeVars.t_chargeSig[3]);
+
 
     //=== make event selection ===
-    if (!SelectEnPoints(treeVars)) continue;
-    if (!SelectEvent_hodo(treeVars)) continue;
-    if (!SelectEvent_scint(treeVars)) continue;
-    //if (!SelectEvent_kov(treeVars)) continue;
+    if (!SelectEnPoints(treeVars,enpoints)) continue;
+    if (!SelectHVPoints(treeVars,hvpoints)) continue;
+    if (!SelectEvent_hodo(treeVars,positionCuts)) continue;
+    if (!SelectEvent_ref(treeVars,mainch,mainthr,refch,refthr)) continue;
+
 
     //timing plots
-    float time = treeVars.t_timeMax[CH]/0.4-treeVars.t_timeMax[CH8]/0.4;
-    h_timeVsAmpli->Fill(treeVars.t_ampMax[CH],time);
+    float time = treeVars.t_timeMax[mainch]/0.4-treeVars.t_timeCFD[trgch]/0.4;
+    h_timeVsAmpli->Fill(treeVars.t_ampMax[mainch],time);
     
-    float timeDiff = treeVars.t_timeMax[CH2]-treeVars.t_timeMax[CH3];
+    float timeDiff = treeVars.t_timeMax[2]-treeVars.t_timeMax[1];
     h_timeDiff->Fill(timeDiff);
     
     char histoName1[100];
     sprintf(histoName1, "h_wf_%f", treeVars.t_beamE);
     if(h_wf.find(treeVars.t_beamE)==h_wf.end())
-      h_wf[treeVars.t_beamE] = new TProfile(histoName1,histoName1,1100,-500.,600.);
+      h_wf[treeVars.t_beamE] = new TProfile(histoName1,histoName1,1300,-500.,800.);
     
     //=== loop over WF samples ===
     for(unsigned int sampl=0; sampl<treeVars.t_nSampl; ++sampl)
-      if(treeVars.t_wfCh[sampl] == CH)
-     	h_wf[treeVars.t_beamE]->Fill(treeVars.t_wfTime[sampl]/0.4-treeVars.t_timeCFD[CH8]/0.4,treeVars.t_wfVal[sampl]);
-    
-    
+      {
+	//std::cout << sampl << " " << treeVars.t_wfCh[sampl] << " " << treeVars.t_wfTime[sampl]/0.4-treeVars.t_timeCFD[trgch]/0.4 << " " << treeVars.t_timeCFD[trgch]  << " " << treeVars.t_wfVal[sampl] << std::endl;
+
+
+	if(treeVars.t_wfCh[sampl] == mainch)
+	  h_wf[treeVars.t_beamE]->Fill(treeVars.t_wfTime[sampl]/0.4-treeVars.t_timeCFD[trgch]/0.4,treeVars.t_wfVal[sampl]);
+      }
+
     char histoName2[100];	
     sprintf(histoName2, "h_ampMax_%f", treeVars.t_beamE);
     if(h_ampMax.find(treeVars.t_beamE)==h_ampMax.end())
-      h_ampMax[treeVars.t_beamE] = new TH1F(histoName2,histoName2,500,0.,4000.);
+      h_ampMax[treeVars.t_beamE] = new TH1F(histoName2,histoName2,80,0.,4000.);
     
-    h_ampMax[treeVars.t_beamE]->Fill(treeVars.t_ampMax[CH]);
-    
-    
+    h_ampMax[treeVars.t_beamE]->Fill(treeVars.t_ampMax[mainch]);
+
   }//end loop
   std::cout << std::endl;
 
@@ -244,14 +268,14 @@ int main(int argc, char** argv)
   //=== draw plots ===
   TCanvas* c_chargeMap = new TCanvas("c_chargeMap","c_chargeMap");
   c_chargeMap->Divide(2,2);
-  c_chargeMap->cd(2);
-  h_chargeMap_ch0->Draw("colz");
   c_chargeMap->cd(1);
+  h_chargeMap_ch0->Draw("colz");
+  c_chargeMap->cd(2);
   h_chargeMap_ch1->Draw("colz");
-  c_chargeMap->cd(3);
-  h_chargeMap_ch3->Draw("colz");
   c_chargeMap->cd(4);
   h_chargeMap_ch2->Draw("colz");
+  c_chargeMap->cd(3);
+  h_chargeMap_ch3->Draw("colz");
 
   TCanvas* c_timeVsAmpli = new TCanvas("c_timeVsAmpli","c_timeVsAmpli");
   c_timeVsAmpli->cd();
@@ -274,8 +298,17 @@ int main(int argc, char** argv)
       iter->second->Fit(myGauss);
       
       //subtract noise. sigma(noise) = 1.85
-      float mySigma = sqrt(myGauss->GetParameter(2)*myGauss->GetParameter(2)-1.85*1.85);
-      resGraph->SetPoint(count,iter->first, mySigma/myGauss->GetParameter(1));
+      //float mySigma = sqrt(myGauss->GetParameter(2)*myGauss->GetParameter(2)-1.85*1.85);
+      float mySigma = myGauss->GetParameter(2);
+      float mySigmaErr = myGauss->GetParError(2);
+      float myMean = myGauss->GetParameter(1);
+      float myMeanErr = myGauss->GetParError(1);
+      resGraph->SetPoint(count,iter->first, mySigma/myMean);
+      float err2 = pow(mySigmaErr/myMean,2) + pow(myMeanErr*mySigma/myMean/myMean,2);
+      resGraph->SetPointError(count, 0., sqrt(err2));
+
+      iter->second->Write();
+
       ++count;
     }
 
@@ -287,18 +320,20 @@ int main(int argc, char** argv)
   resGraph->Fit(myRes);
 
   TCanvas* c_wf = new TCanvas("c_wf","c_wf");
-  c_wf->cd();
+  c_wf->Divide(2,3);
   count = 0;
   for(std::map<float,TProfile*>::const_iterator iter=h_wf.begin(); iter!=h_wf.end(); ++iter)
     {
-      if(count==0)
-	iter->second->Draw();
-      else
-	iter->second->Draw("sames");
+      c_wf->cd(count+1);
+      iter->second->Draw();
+
       ++count;
     }
 
   theApp -> Run();  
+  outFile -> Close();
+  
+
   return 0;
 }
 
